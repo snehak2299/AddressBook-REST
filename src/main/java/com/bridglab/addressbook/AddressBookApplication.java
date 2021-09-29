70e96bd7e8cabf5aeb8fd00ccfa83f794c31d202
snehak2299 <skanase2299@gmail.com>
2021-09-29 16:23:37 +0530

    [sneha] lambok library used for logging

--- src/main/java/com/bridglab/addressbook/AddressBookApplication.java
+++ src/main/java/com/bridglab/addressbook/AddressBookApplication.java
@@ -2,12 +2,18 @@ package com.bridglab.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context=SpringApplication.run(AddressBookApplication.class, args);
+		log.info("address book app started in {} Envoronment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
